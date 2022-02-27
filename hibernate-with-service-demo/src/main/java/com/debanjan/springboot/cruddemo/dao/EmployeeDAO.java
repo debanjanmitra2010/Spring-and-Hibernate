419a90f027c71279b8f4d85b1ca2a3680fc6a1bb
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-26 22:04:05 -0500

    Add files via upload

---
+++ hibernate-with-service-demo/src/main/java/com/debanjan/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.debanjan.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.debanjan.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
